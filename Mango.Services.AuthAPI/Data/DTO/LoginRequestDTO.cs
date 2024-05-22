ba29fcb12553eda80e746b31dc73fd432af33f86
Bat-T <tamal.malda2010@gmail.com>
2024-05-22 11:16:22 +0530

    Add Auth Classes

---
+++ Mango.Services.AuthAPI/Data/DTO/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.AuthAPI.Data.DTO
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
