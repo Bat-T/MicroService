2a247fb8a21812e4ef5de5e0c60701b124b9c747
Bat-T <tamal.malda2010@gmail.com>
2024-07-02 03:34:18 +0530

    product UI addition

---
+++ Mango.Services.CouponAPI/Middlewares/ExceptionToProblemDetailsHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionToProblemDetailsHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionToProblemDetailsHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+
+        //public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        //{
+        //    ///httpContext.Response.Body = JsonSerializer.Serialize(new ProblemDetails() { Detail = exception.Message})
+        //    return new ValueTask<>();
+        //}
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionToProblemDetailsHandler>();
+        }
+    }
+}
--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -9,6 +9,7 @@
         public string CategoryName { get; set; }
         public string? ImageUrl { get; set; }
         public string? ImageLocalPath { get; set; }
+        public int Count { get; set; }
         public IFormFile? Image { get; set; }
     }
 }
